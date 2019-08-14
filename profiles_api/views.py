d708404488629041e411d41e2eccdd071b020fe9
Chandan <p.chandan1995@gmail.com>
2019-08-14 20:17:12 +0530

    APIViews done

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
 
 class HelloAPIView(APIView):
     """Test API view"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Retruns a list of APIView features"""
@@ -14,3 +17,30 @@ class HelloAPIView(APIView):
             'Is mapped manually to URLs',
         ]
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request, format=None):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+    def put(self, request, pk=None):
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'delete'})
+
+
+
+
